9be1ab3022ae5c98522f541a3fa759104659ca49
AshishManer <ashishmaner94@gmail.com>
2022-09-24 13:13:13 +0530

    first commit
    24-9-2022

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+module.exports={
+    MONGOURI:'mongodb://localhost:27017/ashraclone',
+    JWT_SECRET:"fgismlpwasdttgcxzsplhgcxz",
+    SENDGRID_API:"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", 
+    EMAIL:"http:localhost:3000"
+}
